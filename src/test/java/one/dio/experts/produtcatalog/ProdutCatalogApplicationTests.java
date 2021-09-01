4be2ee7f52a2f242fb7be2c8f6c56b83aebc62b4
Diego Jefferson <diegojfsr@gmail.com>
2021-09-01 11:05:14 -0300

    Arquitetura Microsservicos Spring Cloud

---
+++ src/test/java/one/dio/experts/produtcatalog/ProdutCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package one.dio.experts.produtcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
